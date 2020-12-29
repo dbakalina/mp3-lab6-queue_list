7b120dcb026c21112fb51a5b9406f28540d0ee94
dbakalina <darya.bakalina@gmail.com>
2020-12-29 23:48:30 +0300

    Queue_List

--- Queue_list/Queue_list/Queue.h
+++ Queue_list/Queue_list/Queue.h
@@ -1,1 +1,218 @@
 #pragma once
+#include<iostream>
+
+
+template<class T>
+struct TLink 
+{
+	T val;
+	TLink* pNext;
+};
+
+
+template <class T>
+class TQueue
+{
+private:
+	TLink<T>* pHead, * pTail;
+public:
+	TQueue();
+	TQueue(const TQueue<T>& qu);
+	~TQueue();
+	TQueue<T>& operator = (const TQueue<T>& q);
+	bool operator == (const TQueue<T>& q) const;
+	bool operator != (const TQueue<T>& q) const;
+	bool Empty() const;
+	void Push(const T elem);
+	bool Overloaf() const;
+	T SeeHead() const;
+	T GetHead();
+};
+
+
+template<class T>
+TQueue<T>::TQueue()
+{
+	pHead = NULL;
+	pTail = NULL;
+}
+
+
+template <class T>
+TQueue<T>::TQueue(const TQueue<T>& q)
+{
+	if (q.pHead != NULL)
+	{
+		TLink<T>* tmp, * pre;
+		pre = new TLink<T>;
+		pre->val = q.pHead->val;
+		pHead = pre;
+		tmp = q.pHead->pNext;
+		while (tmp != NULL)
+		{
+			TLink<T>* new_link = new TLink<T>;
+			new_link->val = tmp->val;
+			pre->pNext = new_link;
+			pre = new_link;
+			tmp = tmp->pNext;
+		}
+		pTail = pre;
+		pre->pNext = NULL;
+	}
+	else
+	{
+		pHead = NULL;
+		pTail = NULL;
+	}
+}
+
+
+template <class T>
+TQueue<T>::~TQueue()
+{
+	while (pHead != NULL)
+	{
+		TLink<T>* tmp = pHead->pNext;
+		delete pHead;
+		pHead = tmp;
+	}
+}
+
+
+template <class T>
+TQueue<T>& TQueue<T>::operator =(const TQueue<T>& q)
+{
+	if (this != &q)
+	{
+		while (pHead != NULL)
+		{
+			TLink<T>* tmp = pHead->pNext;
+			delete pHead;
+			pHead = tmp;
+		}
+		pTail = NULL;
+		if (q.pHead != NULL)
+		{
+			TLink<T>* pre, * tmp;
+			pre = new TLink<T>;
+			pre->val = q.pHead->val;
+			pHead = pre;
+			tmp = q.pHead->pNext;
+			while (tmp != NULL)
+			{
+				TLink<T>* new_link = new TLink<T>;
+				new_link->val = tmp->val;
+				pre->pNext = new_link;
+				pre = new_link;
+				tmp = tmp->pNext;
+			}
+			pTail = pre;
+			pre->pNext = NULL;
+		}
+	}
+	return (*this);
+}
+
+
+template<class T>
+bool TQueue<T>::operator==(const TQueue<T>& q) const
+{
+	if (this != &q)
+	{
+		TLink<T>* tmp_th, * tmp_par;
+		tmp_th = pHead;
+		tmp_par = q.pHead;
+		while (true)
+		{
+			if (tmp_par == NULL && tmp_th == NULL)
+			{
+				return true;
+			}
+			if (tmp_par == NULL || tmp_th == NULL)
+			{
+				return false;
+			}
+			if (tmp_th->val != tmp_par->val)
+			{
+				return false;
+			}
+			tmp_th = tmp_th->pNext;
+			tmp_par = tmp_par->pNext;
+		}
+	}
+	return true;
+}
+
+
+template<class T>
+bool TQueue<T>::operator!=(const TQueue<T>& q) const
+{
+	return (!(*this == q));
+}
+
+template<class T>
+bool TQueue<T>::Overloaf() const
+{
+	TLink<T>* tmp = new TLink<T>;
+	if (tmp == 0x0)
+	{
+		delete tmp;
+		return true;
+	}
+	delete tmp;
+	return false;
+}
+
+template<class T>
+bool TQueue<T>::Empty() const
+{
+	if (pHead == NULL)
+		return true;
+	return false;
+}
+
+
+template <class T>
+void TQueue<T>::Push(const T elem)
+{
+	if (Overloaf())
+	{
+		throw 0;
+	}
+	TLink<T>* tmp = new TLink<T>;
+	tmp->val = elem;
+	tmp->pNext = NULL;
+	if (Empty())
+	{
+		pHead = tmp;
+		pTail = tmp;
+	}
+	else
+	{
+		pTail->pNext = tmp;
+		pTail = tmp;
+	}
+}
+
+
+template<class T>
+T TQueue<T>::GetHead()
+{
+	if (Empty())
+	{
+		throw 0;
+	}
+	TLink<T>* tmp = pHead->pNext;
+	T elem = pHead->val;
+	delete pHead;
+	pHead = tmp;
+	return elem;
+}
+
+template<class T>
+T TQueue<T>::SeeHead() const
+{
+	if (Empty())
+		throw 0;
+	return pHead->val;
+}
--- Queue_list/test_queue/Test_queue.cpp
+++ Queue_list/test_queue/Test_queue.cpp
@@ -1,178 +1,120 @@
 ﻿#include"gtest.h"
-#include "../Queue/Queue.h"
+#include "../Queue_list/Queue.h"
 
 TEST(Queue, can_create_queue)
 {
-    ASSERT_NO_THROW(TQueue<int> s(8));
-}
-
-TEST(Queue, cant_create_queue_with_negative_MaxSize)
-{
-    ASSERT_ANY_THROW(TQueue<int> s(-5));
-}
-
-TEST(Queue, can_get_size)
-{
-    TQueue<int> q(7);
-    q.Push(7);
-    ASSERT_NO_THROW(q.GetSize());
-    EXPECT_EQ(1, q.GetSize());
+    ASSERT_NO_THROW(TQueue<int> q);
 }
 
 TEST(Queue, the_copied_queue_is_the_same_as_the_original_one)
 {
-    TQueue<int> q(7);
+    TQueue<int> q;
     q.Push(7);
-    TQueue<int> copy(q);
-    EXPECT_EQ(q.GetSize(), copy.GetSize());
-    EXPECT_EQ(q.GetHead(), copy.GetHead());
+    ASSERT_NO_THROW( TQueue<int> copy(q)) ;
 }
 
 TEST(Queue, can_copy_the_queue)
 {
-    TQueue<int> q(7);
+    TQueue<int> q;
     q.Push(7);
     ASSERT_NO_THROW(TQueue<int> copy(q));
 }
 
-TEST(Queue, can_assign_queue_of_the_same_data_types_and_MaxSize)
+TEST(Queue, can_assign_empty_queue)
 {
-    TQueue<int> q1(5);
-    TQueue<int> q2(5);
-    q1.Push(5);
-    q1.Push(7);
-    q2.Push(3);
-    ASSERT_NO_THROW(q2 = q1);
-    EXPECT_EQ(q1.GetSize(), q2.GetSize());
-    EXPECT_EQ(q1.GetHead(), q2.GetHead());
-}
-TEST(Queue, can_assign_queue_of_the_same_data_types_and_different_MaxSize)
-{
-    TQueue<int> q1(8);
-    TQueue<int> q2(5);
-    q1.Push(5);
-    q1.Push(7);
-    q2.Push(3);
-    ASSERT_NO_THROW(q2 = q1);
-    EXPECT_EQ(q1.GetSize(), q2.GetSize());
-    EXPECT_EQ(q1.GetHead(), q2.GetHead());
+    TQueue<int> q1;
+    TQueue<int> q2;
+    ASSERT_NO_THROW(q1 = q2);
 }
 
 TEST(Queue, can_compare_queue)
 {
-    TQueue<int> q1(7);
-    TQueue<int> q2(7);
-    q1.Push(5);
-    q2.Push(3);
+    TQueue<int> q1;
+    TQueue<int> q2;
+    q1.Push(7);
+    q2.Push(4);
     ASSERT_NO_THROW(q2 == q1);
 }
 
 TEST(Queue, can_compare_the_queue_with_itself)
 {
-    TQueue<int> q(7);
+    TQueue<int> q;
     EXPECT_EQ(true, q == q);
 }
 
-TEST(Queue, comparing_the_same_queue_gives_the_truth)
+TEST(Queue, can_compare_the_same_queue)
 {
-    TQueue<int> q1(3);
-    TQueue<int> q2(3);
-    q1.Push(3);
-    q1.Push(3);
-    q1.Push(5);
-    q1.GetHead();
-    q1.GetHead();
-    q1.Push(3);
-    q1.Push(5);
-    q1.GetHead();
-    q1.GetHead();
-    q2.Push(5);
+    TQueue<int> q1;
+    TQueue<int> q2;
+    q1.Push(7);
+    q2.Push(7);
     EXPECT_EQ(true, q1 == q2);
 }
 
 TEST(Queue, comparing_different_queue_gives_the_false)
 {
-    TQueue<int> q1(5);
-    TQueue<int> q2(5);
+    TQueue<int> q1;
+    TQueue<int> q2;
     q1.Push(5);
-    q1.Push(5);
-    q2.Push(5);
     EXPECT_EQ(true, q1 != q2);
 }
 
 TEST(Queue, can_check_the_queue_for_emptiness)
 {
-    TQueue<int> q(5);
+    TQueue<int> q;
     ASSERT_NO_THROW(q.Empty());
 }
 
-TEST(Queue, a_queue_is_not_empty_when_a_size_greater_than_zero)
+TEST(Queue, viewing_the_top_of_the_queue_does_not_change_the_state_of_the_stack)
 {
-    TQueue<int> q(5);
-    q.Push(5);
-    EXPECT_EQ(false, q.Empty());
-}
-
-TEST(Queue, can_check_the_queue_for_fullness)
-{
-    TQueue<int> q(5);
-    ASSERT_NO_THROW(q.Full());
-}
-
-TEST(Queue, if_the_queue_size_is_equal_to_the_MaxSize_then_the_stack_is_full)
-{
-    int max_size = 2;
-    TQueue<int> q(max_size);
-    q.Push(1);
+    TQueue<int> q;
     q.Push(2);
-    EXPECT_EQ(q.GetSize(), max_size);
-    EXPECT_EQ(true, q.Full());
+    int x = q.SeeHead();
+    EXPECT_EQ(x, q.SeeHead());
+}
+
+TEST(Queue, checking_for_the_emptiness_of_a_non_empty_queue_returns_false)
+{
+    TQueue<int> q;
+    q.Push(7);
+    EXPECT_EQ(false, q.Empty());
 }
 
 TEST(Queue, can_add_to_an_incomplete_queue)
 {
-    TQueue<int> q(5);
-    ASSERT_NO_THROW(q.Push(5));
+    TQueue<int> q;
+    ASSERT_NO_THROW(q.Push(3));
 }
 
 TEST(Queue, can_see_what_is_at_the_top_of_a_non_empty_queue)
 {
-    TQueue<int> q(5);
-    q.Push(5);
+    TQueue<int> q;
+    q.Push(3);
     ASSERT_NO_THROW(q.SeeHead());
 }
 
-
 TEST(Queue, cant_look_at_the_top_of_an_empty_queue)
 {
-    TQueue <int> q(5);
-    ASSERT_ANY_THROW(q.GetHead());
+    TQueue <int> q;
+    ASSERT_ANY_THROW(q.SeeHead());
 }
 
 TEST(Queue, can_take_an_element_from_a_non_empty_non_empty_queue)
 {
-    TQueue<int> q(5);
-    q.Push(5);
+    TQueue<int> q;
+    q.Push(3);
     ASSERT_NO_THROW(q.GetHead());
 }
 
 TEST(Queue, cant_take_an_element_from_an_empty_queue)
 {
-    TQueue<int> q(5);
+    TQueue<int> q;
     ASSERT_ANY_THROW(q.GetHead());
 }
 
-TEST(TQueue, cant_put_elem_in_full_queue)
+TEST(Queue, can_correcttly_get_elem_from_the_queue)
 {
-    TQueue<int> q(2);
-    q.Push(1);
-    q.Push(2);
-    ASSERT_ANY_THROW(q.Push(3));
-}
-
-TEST(TQueue, can_correcttly_get_elem_from_the_queue)
-{
-    TQueue<int> q(4);
+    TQueue<int> q;
     q.Push(2);
     EXPECT_EQ(q.GetHead(), 2);
 }
